199fdf9a224fdc16928f54b50acba35644408f9d
Your Name <you@example.com>
2024-08-02 22:22:52 +0530

    aaaaaaaa

--- input.py
+++ input.py
@@ -2,7 +2,7 @@ Mongoconnstr="input mongo coon string here"
 pgconnstr='input postgre server coon string here'
 
 """
-Change the type to incremental to update the data recurring at some time,
+Change the type to   to update the data recurring at some time,
 and update the time difference in minutes to set the time period.
 """
 type='one-time'
