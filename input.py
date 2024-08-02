2b1b79eae7d5a320a023a879570a719938f675db
Your Name <you@example.com>
2024-08-02 21:52:18 +0530

    aaaaaaaaaaaaa

--- input.py
+++ input.py
@@ -1,6 +1,4 @@
 Mongoconnstr="mongodb://temp:temp123@164.52.218.240:27017/qa"
 pgconnstr='postgresql://avnadmin:AVNS_BLwPFVkOSPYj7ACpCMl@pg-24d033c1-mrikal-71db.j.aivencloud.com:12270/test?sslmode=require'
 
-
-
 timediff = 60
